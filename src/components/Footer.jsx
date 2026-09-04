ecba3d8b5eac5815e7c12a0011695fb0874a45cd
Aqeel8211 <m.aqeelch8211@gmail.com>
2026-09-04 19:12:29 +0500

    simplify footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,12 @@
-import { HeartIcon } from '@heroicons/react/24/outline';
-
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
     <footer className="bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-800">
-      <div className="container-padding mx-auto py-6">
-        {/* Simple Copyright Line */}
+      <div className="container-padding mx-auto py-4 sm:py-6">
         <div className="text-center text-gray-600 dark:text-gray-400">
-          <p className="flex items-center justify-center">
-            © {currentYear} Muhammad Aqeel. Made with 
-            <HeartIcon className="h-4 w-4 mx-1 text-red-500" /> 
-            using React & .NET
+          <p className="text-xs sm:text-sm">
+            © {currentYear} Muhammad Aqeel.
           </p>
         </div>
       </div>
